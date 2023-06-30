1bd2b5f36c788c5d16959d909099e620be2992c1
nga150803 <ttnga1508@gmail.com>
2023-06-30 23:34:38 +0700

    nga update

---
+++ src/motion/dataImg.ts
@@ -0,0 +1,5 @@
+export const images = [
+  'https://img.freepik.com/free-psd/3d-interface-website-presentation-mockup-isolated_359791-208.jpg?w=1060&t=st=1687865235~exp=1687865835~hmac=36954c055529a9cd1b5aa7a26da76fe67d5bd32658d6f8bd217c806f594ad817',
+  'https://img.freepik.com/free-photo/homepage-seen-computer-screen_23-2149416723.jpg?w=1380&t=st=1687865327~exp=1687865927~hmac=c5610594c21dadfe1f1d5a31faeeb5c3679afc59d66fa6cd27154cdbb35d3f8d',
+  'https://img.freepik.com/free-photo/homepage-seen-laptop-screen_23-2149416717.jpg?w=1380&t=st=1687865367~exp=1687865967~hmac=b2bb253fabb9de983f7806a4dd88ec8ad1ea68f817453e4a20e8e38b436279c7'
+]
--- src/pages/main/Home/components/Technolory/index.tsx
+++ src/pages/main/Home/components/Technolory/index.tsx
@@ -1,41 +1,16 @@
 
-import { Slide } from 'react-slideshow-image'
 import 'react-slideshow-image/dist/styles.css'
 import classNames from 'classnames/bind'
 import styles from './Technogory.module.scss'
 import { View } from '@/components/shared'
-
+import { Example } from '@/motion/Example'
 const cx = classNames.bind(styles)
-// const spanStyle = {
-//   padding: '20px',
-//   background: 'pink',
-//   color: '#000000',
-//   borderRadius: '1rem'
-// }
 
-// const divStyle = {
-//   display: 'flex',
-//   alignItems: 'center',
-//   justifyContent: 'center',
-//   backgroundSize: 'cover',
-//   height: '300px',
-//   borderRadius: '1rem'
-// }
-const slideImages = [
-  {
-    url: 'https://i.pinimg.com/564x/6f/a9/c3/6fa9c33211ce7c08f2cc4fcef6144b7d.jpg',
-    caption: 'Slide 1'
-  },
-  {
-    url: 'https://i.pinimg.com/564x/b7/9b/87/b79b8778b287d072388f7ac3e2734075.jpg',
-    caption: 'Slide 2'
-  },
-  {
-    url: 'https://i.pinimg.com/originals/b8/4d/71/b84d7101bdeafc175cebb6a282cb6775.gif',
-    caption: 'Slide 3'
-  }
-]
+
+
 const Technogory = () => {
+
+  
   return (
     <View
       className={cx('wrapper__technogory')}
@@ -44,33 +19,18 @@ const Technogory = () => {
       whileInView={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5 }}
     >
-      {/* <View className={cx('wrapper__technogory__title')}>
-            <h1>Công nghệ sử dụng</h1>
-            <p>Chúng tôi cung cấp những dịch vụ website về lĩnh vực giải trí</p>
-        </View>
-        <View className={cx('wrapper__technogory__content')}>
-          <Slide>
-          {slideImages.map((slideImage, index)=> (
-              <View key={index} className={cx('wrapper__technogory__content__image')}>
-                  <img src={slideImage.url} alt="" />
-              </View>
-            ))} 
-          </Slide>
-        </View> */}
+    
 
       <View className={cx('wrapper__technogory__container')}>
-          <View className={cx('wrapper__technogory__title')}>
+          <View className={cx('wrapper__technogory__container__title')}>
             <h1>Công nghệ sử dụng</h1>
             <p>Chúng tôi cung cấp những dịch vụ website về lĩnh vực giải trí</p>
           </View>
-          <View className={cx('wrapper__technogory__content')}>
-            <Slide>
-              {slideImages.map((slideImage, index) => (
-                <View key={index} className={cx('wrapper__technogory__content__image')}>
-                  <img src={slideImage.url} alt='' />
-                </View>
-              ))}
-            </Slide>
+         
+          <View className={cx('wrapper__technogory__container__context')}>
+              <View className={cx('wrapper__technogory__container__context__img')}>
+                <Example/>
+              </View>
           </View>
       </View>
     </View>
